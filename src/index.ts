714a4f5153f75a7913ad9bd52d42ff78af087f4b
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-02-20 03:52:48 +0100

    install ts and drizzle

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import express from 'express';
+import routes from "./routes";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use(routes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
