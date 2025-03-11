641de8214dfdf1bb604203b0e06341fabe51abea
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-03-12 00:11:49 +0100

    alter cluster schema

--- src/db/schema/clusters.ts
+++ src/db/schema/clusters.ts
@@ -1,10 +1,8 @@
-import { integer, pgTable } from 'drizzle-orm/pg-core';
-import { embeddingVector } from '../common';
 import { InferSelectModel } from 'drizzle-orm';
+import { integer, pgTable } from 'drizzle-orm/pg-core';
 
 export const clusters = pgTable('clusters', {
     id: integer().notNull().unique(),// The id is assigned by the cluster generator
-    centroid: embeddingVector("centroid").notNull()
 });
 
 export type Cluster = InferSelectModel<typeof clusters>;
