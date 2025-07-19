eeacabe242b7e920deb28c27de1892f77e326994
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-07-19 14:38:14 +0200

    improve get post error handling

--- src/routes/getPost.ts
+++ src/routes/getPost.ts
@@ -1,4 +1,4 @@
-import { eq } from 'drizzle-orm';
+import { DrizzleError, eq } from 'drizzle-orm';
 import { Request, Response, Router } from 'express';
 import { z } from 'zod';
 import { authRequest } from '../authentication';
@@ -7,6 +7,8 @@ import { posts } from '../db/schema/posts';
 import { candidateColumns } from '../posts/common';
 import { personalizePosts } from '../posts/hydratePosts';
 import { postProcessPosts } from '../posts/postProcessPosts';
+import { DatabaseError } from 'pg';
+import { HttpError } from '../middlewares/errorHandler';
 
 const router = Router();
 
@@ -17,8 +19,17 @@ const GetPostSchema = z.object({
 router.get('/:id', async (req: Request, res: Response) => {
     const { id } = GetPostSchema.parse(req.params)
     const user = await authRequest(req)
-    const [post] = await postProcessPosts(await (personalizePosts(getOnePost(id), user)))
-    res.json({ post })
+    try {
+        const [post] = await postProcessPosts(await (personalizePosts(getOnePost(id), user)))
+        if (!post) throw new HttpError(404, 'Post not found')
+        res.json({ post })
+    }
+    catch (e) {
+        console.log(typeof (e))
+        if (e instanceof DatabaseError && e.routine === "MakeParseError")
+            throw new HttpError(422, "Invalid post ID")
+        throw e
+    }
 });
 
 export function getOnePost(id: string) {
